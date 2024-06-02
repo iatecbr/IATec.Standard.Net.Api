9e509711ff06dbaf30380d74c0adb0b15693173f
Elvys Marcos <elvys.marcos@iatec.com>
2024-06-01 22:02:19 -0300

    cleanup: Project cleanup

--- src/Api/Configurations/Extensions/MigrationExtensions.cs
+++ src/Api/Configurations/Extensions/MigrationExtensions.cs
@@ -5,7 +5,7 @@ public static class MigrationExtensions
     public static WebApplication ApplyMigrations(this WebApplication app)
     {
         if (app.Environment.EnvironmentName is "Local") return app;
-        
+
         return app;
     }
 }
